bead1030439103cede3fe0be5b586c9a92d1debe
wildan <wildan78933@gmail.com>
2023-12-23 16:13:48 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name') }}</title>
+        <link rel="icon" href="https://qwords.com/v2/wp-content/uploads/2022/12/cropped-logo-qw-light-32x32.png" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
